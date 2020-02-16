bd700e60bb9e7a78a8dd800d00d1a73445c25ec6
Ian McCausland <imccausl@gmail.com>
2020-02-16 16:19:10 -0700

    Flatten screenshot

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -71,9 +71,8 @@ const ProjectCardImageContainer = styled("div")`
   overflow: hidden;
   position: relative;
 
-  box-shadow: 0px 9px 24px rgba(0, 0, 0, 0.1);
-  border: 16px solid white;
-  border-radius: 2em;
+  border: 2px solid #f4f4f4;
+  padding: 10px;
   min-height: 400px;
 
   @media (max-width: ${dimensions.maxwidthTablet}px) {
