adfca18e287a72bf265fd3c36c4455b9e722f468
Ian McCausland <ian.mccausland@tophat.com>
2020-02-21 13:49:46 -0500

    Fix: fade out now working

--- src/components/_ui/Overlay.jsx
+++ src/components/_ui/Overlay.jsx
@@ -1,31 +1,38 @@
 import React from "react"
-import { useSpring, animated } from "react-spring"
+import { useTransition, animated } from "react-spring"
 import styled from "@emotion/styled"
 
 const OverlayContainer = styled(animated.div)`
-  display: ${props => (props.visible ? "block" : "none")};
   position: fixed;
   z-index: 1000;
   left: 0;
   top: 0;
   height: 100vh;
   width: 100vw;
-  background-color: rgba(0, 0, 0, 0.5);
+  background: rgba(0, 0, 0, 0.5);
 `
 
 export default props => {
   const { visible, toggleVisible } = props
-  const overlayStyle = useSpring({ opacity: visible ? 1 : 0 })
+  const transitions = useTransition(visible, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  })
   // prevent scrolling under the overlay
   document.body.style.overflow = visible ? "hidden" : "scroll"
 
-  return (
-    <OverlayContainer
-      style={overlayStyle}
-      onClick={toggleVisible}
-      visible={visible}
-    >
-      {props.children}
-    </OverlayContainer>
+  return transitions.map(
+    config =>
+      config.item && (
+        <OverlayContainer
+          style={config.props}
+          key={config.key}
+          onClick={toggleVisible}
+          visible={visible}
+        >
+          {props.children}
+        </OverlayContainer>
+      )
   )
 }
