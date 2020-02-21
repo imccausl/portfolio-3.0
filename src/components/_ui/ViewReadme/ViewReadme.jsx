15b2bba3b7af4a23dc05875834cae8edfc162be5
Ian McCausland <ian.mccausland@tophat.com>
2020-02-21 14:26:47 -0500

    Modify: make view container zoom into view as background fades in

--- src/components/_ui/ViewReadme/ViewReadme.jsx
+++ src/components/_ui/ViewReadme/ViewReadme.jsx
@@ -6,6 +6,7 @@ const ViewContainer = styled(animated.div)`
   background: white;
   width: 60%;
   height: 80%;
+  opacity: 1;
 `
 
 export default props => {
@@ -23,7 +24,7 @@ export default props => {
         transform: x
           .interpolate({
             range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
-            output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+            output: [0, 0.5, 0.9, 1],
           })
           .interpolate(x => `scale(${x})`),
       }}
