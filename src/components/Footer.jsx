e098d7216e087e9f2776df06244e87e7d7c1d255
Ian McCausland <imccausl@gmail.com>
2020-02-23 14:25:26 -0500

    Modify: pull social media links into their own component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,8 @@ import React from "react"
 import { Link } from "gatsby"
 import styled from "@emotion/styled"
 import colors from "styles/colors"
-import { SocialMediaIconsReact } from "social-media-icons-react"
+
+import SocialMediaMenu from "./_ui/SocialMediaMenu"
 
 const FooterContainer = styled("div")`
   padding-bottom: 3em;
@@ -33,29 +34,8 @@ const FooterAuthor = styled("div")`
   text-decoration: none;
 `
 
-const SocialContainer = styled("div")`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  justify-content: center;
-  align-items: center;
-  margin-top: 15px;
-  margin-bottom: 10px;
-
-  a {
-    margin-right: 10px;
-  }
-
-  a:last-of-type {
-    margin-right: 0;
-  }
-`
-
 const Footer = props => {
-  console.log(props)
-  const {
-    social: { github, codepen, linkedin },
-  } = props
+  const { social } = props
 
   return (
     <FooterContainer>
@@ -65,41 +45,7 @@ const Footer = props => {
           <span style={{ color: colors.blue300 }}>.</span>land
         </h3>
       </Link>
-      <SocialContainer>
-        <SocialMediaIconsReact
-          borderColor="rgba(0,0,0,0.25)"
-          borderWidth="5"
-          borderStyle="solid"
-          icon="github"
-          iconColor="rgba(255,255,255,1)"
-          iconSize="5"
-          roundness="50%"
-          url={`https://www.github.com/${github}`}
-          size="50"
-        />
-        <SocialMediaIconsReact
-          borderColor="rgba(0,0,0,0.25)"
-          borderWidth="5"
-          borderStyle="solid"
-          icon="codepen"
-          iconColor="rgba(255,255,255,1)"
-          iconSize="5"
-          roundness="50%"
-          url={`https://www.codepen.io/${codepen}`}
-          size="50"
-        />
-        <SocialMediaIconsReact
-          borderColor="rgba(0,0,0,0.25)"
-          borderWidth="5"
-          borderStyle="solid"
-          icon="linkedin"
-          iconColor="rgba(255,255,255,1)"
-          iconSize="5"
-          roundness="50%"
-          url={`https://www.linkedin.com/in/${linkedin}`}
-          size="50"
-        />
-      </SocialContainer>
+      <SocialMediaMenu socialUsernames={social} />
       <FooterAuthor>© 2019</FooterAuthor>
     </FooterContainer>
   )
---
+++ src/components/_ui/SocialMediaMenu.jsx
@@ -0,0 +1,59 @@
+import React from "react"
+import { SocialMediaIconsReact } from "social-media-icons-react"
+import styled from "@emotion/styled"
+
+const SocialContainer = styled("div")`
+  display: flex;
+  flex-direction: ${props => props.direction};
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+  margin-top: 15px;
+  margin-bottom: 10px;
+
+  .icon {
+    ${props => (props.direction === "row" ? "margin-right: 10px;" : "")}
+  }
+
+  .icon:first-of-type {
+    ${props => (props.direction === "row" ? "margin-right: 0" : "")}
+  }
+`
+
+const makeSocialLinks = (data, direction) => {
+  const urlMap = {
+    github: { url: "https://www.github.com/", order: 1 },
+    codepen: { url: "https://www.codepen.io/", order: 2 },
+    twitter: { url: "https://www.twitter.com/", order: 4 },
+    linkedin: { url: "https://www.linkedin.com/in/", order: 3 },
+  }
+
+  return Object.keys(data).map(social => {
+    return (
+      <div className="icon" style={{ order: urlMap[social].order }}>
+        <SocialMediaIconsReact
+          key={`${social}-${data[social]}`}
+          borderColor="rgba(0,0,0,0.25)"
+          borderWidth="5"
+          borderStyle="solid"
+          icon={`${social}`}
+          iconColor="rgba(255,255,255,1)"
+          iconSize="6"
+          roundness="50%"
+          url={`${urlMap[social].url}${data[social]}`}
+          size={direction === "row" ? "50" : "35"}
+        />
+      </div>
+    )
+  })
+}
+
+export default ({ socialUsernames, direction }) => {
+  const flowDirection = direction ? direction : "row"
+
+  return (
+    <SocialContainer direction={flowDirection}>
+      {makeSocialLinks(socialUsernames, flowDirection)}
+    </SocialContainer>
+  )
+}
