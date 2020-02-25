ce5da359c0aca6441f732c0eaf5e367b01e405d2
Ian McCausland <imccausl@gmail.com>
2020-02-24 19:49:18 -0500

    Use react-headroom instead of trying to roll my own header revealer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from "react"
-import { useSpring, animated } from "react-spring"
+import React from "react"
+import Headroom from "react-headroom"
 import { Link } from "gatsby"
 import styled from "@emotion/styled"
 import colors from "styles/colors"
 
 const HeaderContainer = styled("div")`
-  position: static;
-  transition: transform 300ms linear;
+  position: relative;
   top: 0;
   left: 0;
   height: 3em;
@@ -42,56 +41,19 @@ const HeaderStyle = styled("h1")`
 `
 
 const Header = () => {
-  const [isHeaderVisible, setHeaderVisible] = useState(false)
-  const [lastScrollTop, setLastScrollTop] = useState(0)
-  // const headerProps = useSpring({
-  //   top: isHeaderVisible ? 0 : -1,
-  //   position: isHeaderVisible ? "fixed" : "static",
-  // })
-  const handleScroll = () => {
-    const st = window.pageYOffset || document.body.scrollTop
-
-    setHeaderVisible(st < lastScrollTop)
-    setLastScrollTop(st <= 0 ? 0 : st)
-  }
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll)
-
-    return () => {
-      window.removeEventListener("scroll", () => handleScroll)
-    }
-  }, [lastScrollTop])
-
-  const scrollTopThresholdReached = lastScrollTop >= 30
-  let transformValue = "0%"
-
-  if (
-    (isHeaderVisible && scrollTopThresholdReached) ||
-    (isHeaderVisible && !scrollTopThresholdReached) ||
-    (!isHeaderVisible && !scrollTopThresholdReached)
-  ) {
-    transformValue = "0%"
-  } else {
-    transformValue = "-100%"
-  }
   return (
-    <HeaderContainer
-      delay={300}
-      style={{
-        position: scrollTopThresholdReached ? "fixed" : "absolute",
-        transform: `translateY(${transformValue})`,
-      }}
-    >
-      <HeaderContent>
-        <HeaderStyle>
-          <Link to="/#top">
-            <span style={{ color: colors.blue700 }}>ian</span>mccaus
-            <span style={{ color: colors.blue300 }}>.</span>land
-          </Link>
-        </HeaderStyle>
-      </HeaderContent>
-    </HeaderContainer>
+    <Headroom>
+      <HeaderContainer>
+        <HeaderContent>
+          <HeaderStyle>
+            <Link to="/#top">
+              <span style={{ color: colors.blue700 }}>ian</span>mccaus
+              <span style={{ color: colors.blue300 }}>.</span>land
+            </Link>
+          </HeaderStyle>
+        </HeaderContent>
+      </HeaderContainer>
+    </Headroom>
   )
 }
 
